8c76fce951435f315025f0031a90513f28954374
汝果67 <3207795529@qq.com>
2024-07-16 22:15:41 +0800

    api模块封装-axios配合ts使用

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom/client'
-import './index.css'
 
 // 导入provider
 import { RouterProvider } from 'react-router-dom'
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,35 @@
-const Home = () => {
-    return <div>this is home</div>
-}
+import { Tabs } from "antd-mobile";
+import "./style.css";
+import { useEffect, useState } from "react";
+import type { ChannelItem } from "@/apis/shared";
+import { fetchChannelAPI } from "@/apis/list";
 
-export default Home
+const Home = () => {
+  const [channels, setChannels] = useState<ChannelItem[]>([]);
+  useEffect(() => {
+    const getChannels = async () => {
+      try {
+        const res = await fetchChannelAPI();
+        setChannels(res.data.data.channels);
+      } catch (error) {
+        throw new Error("fetch channel error");
+      }
+    };
+    getChannels();
+  }, []);
+  return (
+    <div>
+      <div className="tabContainer">
+        <Tabs>
+          {channels.map((item) => (
+            <Tabs.Tab title={item.name} key={item.id}>
+              {item.name}
+            </Tabs.Tab>
+          ))}
+        </Tabs>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
